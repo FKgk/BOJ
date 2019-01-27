d95d04030545b26ce09b4e68488190769a495f77
yellow Wing <40608930+FKgk@users.noreply.github.com>
2019-01-27 22:35:55 +0900

    이친수

--- 2193.c
+++ 2193.c
@@ -2,15 +2,14 @@
 
 int main(void)
 {
-    long long int dp[92];
-    dp[0] = 0;
-    dp[1] = 1;
+    long long int dp[92] = {0, 1, };
     int n;
+    
     scanf("%d", &n);
+    
     for(int i =2; i <= n;i++)
-    {
         dp[i] = dp[i-1] + dp[i-2];
-    }
+    
     printf("%lld", dp[n]);
     
     return 0;
