361b4fd3b574e8b57cadf3465dc3c4a52e3bb6b2
Kwangho Kim <rhkd865@naver.com>
2019-04-28 02:48:05 +0900

    걷다보니 신천역 삼 (Small)

--- 14650.cpp
+++ 14650.cpp
@@ -1,27 +1,14 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int dp[100][3];
 int main(void)
 {
 	int n;
-
 	cin >> n;
-	dp[1][0] = 0;
-	dp[1][2] = dp[1][1] = 1;
 
-	for (int i = 2; i <= n; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			for (int k = 0; k < 3; k++)
-			{
-				int t = (dp[i - 1][j] + k) % 3;
-			
-				dp[i][t] += dp[i - 1][j];
-			}
-		}
-	}
-	
-	cout << dp[n][0] << endl;
+	if (n == 1)
+		cout << "0"<<endl;
+	else
+		cout << 2 * pow(3, n -2) <<endl;
 }
